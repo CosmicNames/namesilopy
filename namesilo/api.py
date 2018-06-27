39973364761749a11cce4f6e5481b67d47a3ea4c
Eric Hocking <xaoseric@users.noreply.github.com>
2018-06-27 12:39:18 -0400

    new error handling methods

--- namesilo/api.py
+++ namesilo/api.py
@@ -1,11 +1,57 @@
 import requests
 from bs4 import BeautifulSoup
+import namesilo.errors as nserrors
 
 class NameSiloAPI(object):
 
-    def __init__(self, apiKey=None):
+    LIVE_BASE_URL = 'https://www.namesilo.com/api/'
+    SANDBOX_BASE_URL = 'http://sandbox.namesilo.com/api/'
+
+    NAMESILO_OPERATIONS = {
+        'add_account_funds': 'addAccountFunds',
+        'add_auto_renew': 'addAutoRenewal',
+        'add_contact': 'contactAdd',
+        'add_dns_record': 'dnsAddRecord',
+        'add_email_forward': 'configureEmailForward',
+        'add_portfolio': 'portfolioAdd',
+        'add_privacy': 'addPrivacy',
+        'add_registered_nameserver': 'addRegisteredNameServer',
+        'associate_contact': 'contactDomainAssociate',
+        'associate_portfolio': 'portfolioDomainAssociate',
+        'change_nameservers': 'changeNameServers',
+        'check_register_availability': 'checkRegisterAvailability',
+        'check_transfer_availability': 'checkTransferAvailability',
+        'check_transfer_status': 'checkTransferStatus',
+        'delete_dns_record': 'dnsDeleteRecord',
+        'delete_portfolio': 'portfolioDelete',
+        'delete_registered_nameserver': 'deleteRegisteredNameServer',
+        'forward_domain': 'domainForward',
+        'get_account_balance': 'getAccountBalance',
+        'get_auth_code': 'retrieveAuthCode',
+        'get_domain_info': 'getDomainInfo',
+        'list_contacts': 'contactList',
+        'list_dns_records': 'dnsListRecords',
+        'list_domains': 'listDomains',
+        'list_email_forwards': 'listEmailForwards',
+        'list_portfolios': 'portfolioList',
+        'list_registered_nameservers': 'listRegisteredNameServers',
+        'lock_domain': 'domainLock',
+        'register_domain': 'registerDomain',
+        'renew_domain': 'renewDomain',
+        'remove_auto_renewal': 'removeAutoRenewal',
+        'remove_email_forward': 'deleteEmailForward',
+        'remove_privacy': 'removePrivacy',
+        'transfer_domain': 'ransferDomain',
+        'unlock_domain': 'domainUnlock',
+        'update_contact': 'contactUpdate',
+        'update_dns_record': 'dnsUpdateRecord',
+        'update_portfolio': 'portfoliopdate',
+        'update_registered_nameserver': 'modifyRegisteredNameServer'
+    }
+
+    def __init__(self, apiKey=None, live=True):
         self.apiKey = apiKey
-        self.baseUri = "https://www.namesilo.com/api/"
+        self.baseUri = self.LIVE_BASE_URL if live else self.SANDBOX_BASE_URL
 
         if self.apiKey is None:
             raise ValueError('No Api Key present')
@@ -16,7 +62,32 @@ class NameSiloAPI(object):
             'key': self.apiKey
         }
     
+    def request(self, operation, **kwargs):
+        operation = self.NAMESILO_OPERATIONS.get(operation, operation)
+        data = self.params
+
+        if not kwargs:
+            raise ValueError('No arguments provided.')
+        
+        for key, value in kwargs.items():
+            data[key] = value
+        
+        result = requests.get(self.baseUri + operation, params=data)
+        soup = BeautifulSoup(result.content, 'lxml-xml')
+        self.handle_error(soup)
+
+        return soup
+
+    def handle_error(self, soup):
+        code = soup.namesilo.reply.code
+        print(code.string)
+        if code.string in nserrors.NAMESILO_ERRORS:
+            error = nserrors.NAMESILO_ERRORS[code.string]
+            detail = soup.namesilo.reply.detail
+            raise error(detail.string)
+
     def checkRegisterAvailability(self, *domains):
+        data = self.params
         query = ""
 
         if not domains:
@@ -28,11 +99,12 @@ class NameSiloAPI(object):
             else:
                 query += domain
         
-        self.params['domains'] = query
-        print(self.params) # Debug
+        data['domains'] = query
+        print(data) # Debug
 
-        result = requests.get(self.baseUri + "checkRegisterAvailability", params=self.params)
+        result = requests.get(self.baseUri + "checkRegisterAvailability", params=data)
         soup = BeautifulSoup(result.content, 'lxml-xml')
+        self.handle_error(soup)
 
         return soup
     
